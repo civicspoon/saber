d34e48dbee3675c64113102c17eec4d9b7d350c6
Tock <kaewsumran.p@gmail.com>
2024-07-24 14:13:33 +0700

    แนททระ

--- app/Components/Print/Summary/page.js
+++ app/Components/Print/Summary/page.js
@@ -1,11 +1,13 @@
-// page.js
 import React, { Suspense } from 'react';
+import Head from 'next/head'; // Import Head from next/head
 import ClientComponent from './ClientComponent';
 
 export default function Page() {
     return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <ClientComponent />
-        </Suspense>
+        <>
+            <Suspense fallback={<div>Loading...</div>}>
+                <ClientComponent />
+            </Suspense>
+        </>
     );
 }
