5cedcb209834c81530681823fa95996fbfe7ed1a
Tock <kaewsumran.p@gmail.com>
2024-07-25 14:01:42 +0700

    admin Password chang compltet

---
+++ app/Admin/ChangePassword/page.js
@@ -0,0 +1,11 @@
+import ChangPassword from "@/app/Components/ChangPassword";
+
+function page() {
+    return (
+        <div className="flex justify-center items-center">
+            <ChangPassword />
+        </div>
+    );
+}
+
+export default page;
